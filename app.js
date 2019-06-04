7a4857255ea0c5166e460a6ac6dde9d1899d65f0
wanaka <asd8116@gmail.com>
2019-06-04 14:34:17 +0800

    Install bcrypt-nodejs & body-praser,Add userController and signup page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   db.sequelize.sync()
