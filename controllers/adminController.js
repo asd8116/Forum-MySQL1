98d1703d4fa8e2e2657e4c0dbb0ec284613b128d
wanaka <asd8116@gmail.com>
2019-06-04 16:42:21 +0800

    Add admin restaurant create form and post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,7 +4,28 @@ const Restaurant = db.Restaurant
 const adminController = {
   getRestaurants: (req, res) => {
     return Restaurant.findAll().then(restaurants => {
-      return res.render('admin/restaurants', { restaurants: restaurants })
+      return res.render('admin/restaurants', { restaurants: restaurants, user: req.user, isAuthenticated: req.isAuthenticated })
+    })
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description
+    }).then(restaurant => {
+      req.flash('success_messages', 'restaurant was successfully created')
+      res.redirect('/admin/restaurants')
     })
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,8 @@ module.exports = (app, passport) => {
 
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
