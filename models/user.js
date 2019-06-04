0bf71159466d6968fb58dec4a6b7292b13a9b9cc
wanaka <asd8116@gmail.com>
2019-06-04 16:06:46 +0800

    Add isAdmin to users table and authentication

--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      isAdmin: DataTypes.BOOLEAN
+    },
+    {}
+  )
   User.associate = function(models) {
     // associations can be defined here
-  };
-  return User;
-};
+  }
+  return User
+}
