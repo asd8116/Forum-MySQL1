9a3edfda29ea51ea14fbcc5b15d87122edec608a
wanaka <asd8116@gmail.com>
2019-06-04 14:54:55 +0800

    Install express-session connect-flash,Confirm user password

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
