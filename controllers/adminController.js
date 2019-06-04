c2cb5ac2e417b812fb01bb6d23da2714f09ce053
wanaka <asd8116@gmail.com>
2019-06-04 16:52:04 +0800

    Add admin restaurant page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -27,6 +27,12 @@ const adminController = {
       req.flash('success_messages', 'restaurant was successfully created')
       res.redirect('/admin/restaurants')
     })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id).then(restaurant => {
+      return res.render('admin/restaurant', { restaurant: restaurant })
+    })
   }
 }
 
