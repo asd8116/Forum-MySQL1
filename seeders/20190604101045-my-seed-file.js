92e1b650f6eff49bd47dfc12f636d9a8551b331a
wanaka <asd8116@gmail.com>
2019-06-11 12:13:55 +0800

    Add category seed data

--- seeders/20190604101045-my-seed-file.js
+++ seeders/20190604101045-my-seed-file.js
@@ -35,6 +35,17 @@ module.exports = {
       {}
     )
 
+    queryInterface.bulkInsert(
+      'Categories',
+      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理'].map((item, index) => ({
+        id: index + 1,
+        name: item,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
     return queryInterface.bulkInsert(
       'Restaurants',
       Array.from({ length: 50 }).map(d => ({
@@ -45,7 +56,8 @@ module.exports = {
         image: faker.image.imageUrl(),
         description: faker.lorem.text(),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        CategoryId: Math.floor(Math.random() * 5) + 1
       })),
       {}
     )
@@ -53,6 +65,7 @@ module.exports = {
 
   down: (queryInterface, Sequelize) => {
     queryInterface.bulkDelete('Users', null, {})
+    queryInterface.bulkDelete('Categories', null, {})
     return queryInterface.bulkDelete('Restaurants', null, {})
   }
 }
