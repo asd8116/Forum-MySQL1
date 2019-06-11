e60da455b96d3f5db142a10784d75bbb2a565771
wanaka <asd8116@gmail.com>
2019-06-11 13:58:01 +0800

    Add admin categories delete action

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -36,6 +36,14 @@ const categoryController = {
         })
       })
     }
+  },
+
+  deleteCategory: (req, res) => {
+    return Category.findByPk(req.params.id).then(category => {
+      category.destroy().then(category => {
+        res.redirect('/admin/categories')
+      })
+    })
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,9 @@ module.exports = (app, passport) => {
 
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
+  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
   app.get('/admin/users', authenticatedAdmin, userController.editUser)
   app.put('/admin/users/:id', authenticatedAdmin, userController.putUser)
