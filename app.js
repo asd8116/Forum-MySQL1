448de21adb17bbba0ddf14a6cac1212ca92720bc
wanaka <asd8116@gmail.com>
2019-06-11 13:09:09 +0800

    Add handlebar helper ifCond and modify category select

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', handlebars({ defaultLayout: 'main', helpers: require('./config/handlebars-helpers') }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
