564560a95643267996e1aa97cbac4bc4c1feb711
wanaka <asd8116@gmail.com>
2019-06-11 17:44:15 +0800

    Show related comments and using moment

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 const pageLimit = 10
 
 const restController = {
@@ -35,7 +37,7 @@ const restController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { include: Category }).then(restaurant => {
+    return Restaurant.findByPk(req.params.id, { include: [Category, { model: Comment, include: [User] }] }).then(restaurant => {
       return res.render('restaurant', { restaurant: restaurant })
     })
   }
