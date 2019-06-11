e9fa621bb20837a2be26c80ab32424bf1007626b
wanaka <asd8116@gmail.com>
2019-06-11 19:27:28 +0800

    Adjustment symbol

--- migrations/20190604081455-create-restaurant.js
+++ migrations/20190604081455-create-restaurant.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Restaurants', {
@@ -31,9 +31,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Restaurants');
+    return queryInterface.dropTable('Restaurants')
   }
-};
+}
