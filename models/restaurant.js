a26c6ba3d22a90bac30ed91f5f08389d4649fe84
wanaka <asd8116@gmail.com>
2019-06-11 12:03:28 +0800

    Create category model,Add restaurant & category and association

--- models/restaurant.js
+++ models/restaurant.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
     {}
   )
   Restaurant.associate = function(models) {
-    // associations can be defined here
+    Restaurant.belongsTo(models.Category)
   }
   return Restaurant
 }
