5e712877721264d853105fc1bb3f05816566abfd
wanaka <asd8116@gmail.com>
2019-06-11 15:53:37 +0800

    Add categories navbar and filter and active item

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,12 +4,22 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['categoryId'] = categoryId
+    }
+
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
-      return res.render('restaurants', { restaurants: data })
+
+      Category.findAll().then(categories => {
+        return res.render('restaurants', { restaurants: data, categories: categories, categoryId: categoryId })
+      })
     })
   },
 
