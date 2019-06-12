7a4f1d687a89ce89aea21ec54ce8f4c65f7cb179
wanaka <asd8116@gmail.com>
2019-06-12 16:14:12 +0800

    Show commented restaurants on profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
+const Comment = db.Comment
+const Restaurant = db.Restaurant
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
@@ -67,7 +69,7 @@ const userController = {
   },
 
   getUser: (req, res) => {
-    return User.findByPk(req.params.id).then(user => {
+    return User.findByPk(req.params.id, { include: [{ model: Comment, include: [Restaurant] }] }).then(user => {
       return res.render('users/profile', { profile: user })
     })
   },
