72aca991ab4544dfdb2f12d3b64844e15ba49161
wanaka <asd8116@gmail.com>
2019-06-12 14:05:54 +0800

    Add image column to users table

---
+++ migrations/20190612055736-add-image-to-user.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Users', 'image', {
+      type: Sequelize.STRING
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Users', 'image')
+  }
+}
