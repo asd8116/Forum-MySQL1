25aabf96f2ed8c59cf017e251681215e68c60274
wanaka <asd8116@gmail.com>
2019-06-17 17:39:13 +0800

    Create like model between restaurant and user

--- models/user.js
+++ models/user.js
@@ -18,6 +18,11 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: 'UserId',
       as: 'FavoritedRestaurants'
     })
+    User.belongsToMany(models.Restaurant, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedRestaurants'
+    })
   }
   return User
 }
